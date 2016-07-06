64afd2ebb65da34b5ed61701aa1070872feeabaa
Shibbir Ahmed <shibbir.io@outlook.com>
2016-07-06 23:09:40 +0600

    Fix bug in app.component.ts

--- generators/app/templates/src/app/app.component.ts
+++ generators/app/templates/src/app/app.component.ts
@@ -2,10 +2,10 @@ import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-<% if (webpack && bootstrap || foundation) { %>
+import {AboutComponent} from './about/about.component';<% if (webpack) { %>
+<% if (bootstrap || foundation) { %>
 <% if (bootstrap) { %>import 'bootstrap/dist/css/bootstrap.css';<% } %><% if (foundation) { %>import 'foundation-sites/dist/foundation.css';<% } %><% } %>
-<% if (webpack) { %>import '../styles.css';<% } %>
+import '../styles.css';<% } %>
 
 @Component({
     selector: 'my-app',
