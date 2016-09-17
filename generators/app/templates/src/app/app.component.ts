698484246e00fe807650a7553469b7b2176909d8
Shibbir Ahmed <shibbir.io@outlook.com>
2016-09-18 03:24:12 +0600

    Fixes #16

--- generators/app/templates/src/app/app.component.ts
+++ generators/app/templates/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import {Component} from '@angular/core';<% if (webpack) { %>
+import {Component, OnInit} from '@angular/core';<% if (webpack) { %>
 <% if (bootstrap || foundation) { %>
 <% if (bootstrap) { %>import 'bootstrap/dist/css/bootstrap.css';<% } %><% if (foundation) { %>import 'foundation-sites/dist/foundation.css';<% } %><% } %>
-import '../styles.css';<% } %>
+import '../styles.css';<% } %><% if(foundation) { %>
+
+declare let $: any;<% } %>
 
 @Component({
     selector: 'my-app',
     <% if (webpack) { %>template: require('./app.component.html')<% } else { %>templateUrl: './app/app.component.html'<% } %>
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+    ngOnInit() {
+        console.log('AppComponent initializing...');<% if(foundation) { %>
+        $(document).foundation();<% } %>
+    }
 }
